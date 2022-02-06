94816c1ae20959986adb6a8870d47f0e65f60b7d
hamesjan <jhan31903@gmail.com>
2022-02-05 21:43:38 -0800

    API keys config
    added .gitignore and hid api keys to twitterbot

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import tweepy;
+import config.py;
 
 
 def main():
-    auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-    auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
     # Create API object
     api = tweepy.API(auth)
 
     # Create a tweet
     api.update_status("Hello Tweepy")
-    
+    print("ok")
+
 if __name__ == "__main__":
     main()
